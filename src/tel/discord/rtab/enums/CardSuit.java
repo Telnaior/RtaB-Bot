adbe65481c1cd13081abce548cd2fa4f9163ad8b
StrangerCoug <strangercoug@hotmail.com>
2018-07-24 20:59:00 -0600

    Removed redundant functions in Card.java; updated CardSuit.java

--- src/tel/discord/rtab/enums/CardSuit.java
+++ src/tel/discord/rtab/enums/CardSuit.java
@@ -1,6 +1,8 @@
 package tel.discord.rtab.enums;
 
-// Swiped and slightly modified from another project of mine I did for university. --Coug
+// Swiped and slightly modified from another project of mine I did for university.
+// Order shouldn't matter *TOO* much here so long as it's also updated in Card.java. --Coug
+
 public enum CardSuit {
     CLUBS,DIAMONDS,HEARTS,SPADES
 }
