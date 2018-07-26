Telnaior/RtaB-Bot

commit 87ae03c3f7b92d6ab52a3aea196526259316c5a6

Deck is now its own object

src/tel/discord/rtab/minigames/DeucesWild.java
@@ -180,18 +180,6 @@ public class DeucesWild implements MiniGame {
 		return BONUS;
 	}
 
-	public Card[] createDeck() {
-		CardRank[] ranks = CardRank.values();
-		CardSuit[] suits = CardSuit.values();
-		Card[] cards = new Card[ranks.length * suits.length];
-
-		for (int i = 0; i < cards.length; i++) {
-			cards[i] = new Card(ranks[i/suits.length],suits[i%suits.length]);
-		}
-
-		return cards;
-	}
-
 	// This is probably not the most efficient way to write the hand evaluator--some things are checked more than once. 
 	public PokerHand evaluateHand(Card[] cards) {
 		if (cards.length != 5)
@@ -289,16 +277,7 @@ public class DeucesWild implements MiniGame {
 	}
 
 	private boolean hasExtraPair(byte[] rankCount) {
-		/* 
-		 * This works, but isn't entirely clear why:
-		 * There can only be a maximum of one deuce in a full house
-		 * And if there is one, it's pair + pair + deuce
-		 * Otherwise the result would be four or five of a kind and we wouldn't get to this point anyway
-		 * In any case, in a full house sorting the array comes out as either (0,2,3) or (1,2,2)
-		 * And the second-to-last value would always be 2.
-		 */
-		byte[] sortedRankCount = rankCount;
-		Arrays.sort(sortedRankCount);
-		return sortedRankCount[rankCount.length - 2] == 2;
+		Arrays.sort(rankCount);
+		return rankCount[rankCount.length - 2] == 2;
 	}
 }

src/tel/discord/rtab/objs/Deck.java
@@ -1,0 +1,42 @@
+package tel.discord.rtab.objs;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import tel.discord.rtab.enums.CardRank;
+import tel.discord.rtab.enums.CardSuit;
+
+public class Deck {
+	LinkedList<Card> deck;
+
+	public Deck() {
+		this(1);
+	}
+
+	public Deck(int numDecks) {
+		deck = new LinkedList<>();
+		addDecks(numDecks);
+	}
+
+	public void addDecks(int numDecks) {
+		CardRank[] ranks = CardRank.values();
+		CardSuit[] suits = CardSuit.values();
+		
+		for (int i = 0; i < numDecks; i++) {
+			for (int j = 0; j < ranks.length * suits.length; j++) {
+				deck.add(new Card(ranks[j/suits.length],suits[j%suits.length]));
+			}
+		}
+	}
+
+	public void clear() {
+		deck.clear();
+	}
+
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+
+	public Card dealCard() {
+		return deck.pop();
+	}
+}

commit aa2cdc66854e6bc63e6aa5e6443e66d1e58040b3

Commit 87ae03c accidentally undid commit eaed373; this reinstates the change from there

src/tel/discord/rtab/minigames/DeucesWild.java
@@ -277,7 +277,16 @@ public class DeucesWild implements MiniGame {
 	}
 
 	private boolean hasExtraPair(byte[] rankCount) {
-		Arrays.sort(rankCount);
-		return rankCount[rankCount.length - 2] == 2;
+		/* 
+ 		 * This works, but isn't entirely clear why:
+ 		 * There can only be a maximum of one deuce in a full house
+ 		 * And if there is one, it's pair + pair + deuce
+ 		 * Otherwise the result would be four or five of a kind and we wouldn't get to this point anyway
+  		 * In any case, in a full house sorting the array comes out as either (0,2,3) or (1,2,2)
+ 		 * And the second-to-last value would always be 2.
+ 		 */
+ 		byte[] sortedRankCount = rankCount;
+ 		Arrays.sort(sortedRankCount);
+ 		return sortedRankCount[rankCount.length - 2] == 2;
 	}
 }