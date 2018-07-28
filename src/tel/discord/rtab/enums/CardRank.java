Telnaior/RtaB-Bot

commit 87ae03c3f7b92d6ab52a3aea196526259316c5a6

Deck is now its own object

src/tel/discord/rtab/minigames/DeucesWild.java
@@ -180,18 +180,6 @@ public class DeucesWild implements MiniGame {
 		return BONUS;
 	}
 
-	public Card[] createDeck() {
-		CardRank[] ranks = CardRank.values();
-		CardSuit[] suits = CardSuit.values();
-		Card[] cards = new Card[ranks.length * suits.length];
-
-		for (int i = 0; i < cards.length; i++) {
-			cards[i] = new Card(ranks[i/suits.length],suits[i%suits.length]);
-		}
-
-		return cards;
-	}
-
 	// This is probably not the most efficient way to write the hand evaluator--some things are checked more than once. 
 	public PokerHand evaluateHand(Card[] cards) {
 		if (cards.length != 5)
@@ -289,16 +277,7 @@ public class DeucesWild implements MiniGame {
 	}
 
 	private boolean hasExtraPair(byte[] rankCount) {
-		/* 
-		 * This works, but isn't entirely clear why:
-		 * There can only be a maximum of one deuce in a full house
-		 * And if there is one, it's pair + pair + deuce
-		 * Otherwise the result would be four or five of a kind and we wouldn't get to this point anyway
-		 * In any case, in a full house sorting the array comes out as either (0,2,3) or (1,2,2)
-		 * And the second-to-last value would always be 2.
-		 */
-		byte[] sortedRankCount = rankCount;
-		Arrays.sort(sortedRankCount);
-		return sortedRankCount[rankCount.length - 2] == 2;
+		Arrays.sort(rankCount);
+		return rankCount[rankCount.length - 2] == 2;
 	}
 }

src/tel/discord/rtab/objs/Deck.java
@@ -1,0 +1,42 @@
+package tel.discord.rtab.objs;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import tel.discord.rtab.enums.CardRank;
+import tel.discord.rtab.enums.CardSuit;
+
+public class Deck {
+	LinkedList<Card> deck;
+
+	public Deck() {
+		this(1);
+	}
+
+	public Deck(int numDecks) {
+		deck = new LinkedList<>();
+		addDecks(numDecks);
+	}
+
+	public void addDecks(int numDecks) {
+		CardRank[] ranks = CardRank.values();
+		CardSuit[] suits = CardSuit.values();
+		
+		for (int i = 0; i < numDecks; i++) {
+			for (int j = 0; j < ranks.length * suits.length; j++) {
+				deck.add(new Card(ranks[j/suits.length],suits[j%suits.length]));
+			}
+		}
+	}
+
+	public void clear() {
+		deck.clear();
+	}
+
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+
+	public Card dealCard() {
+		return deck.pop();
+	}
+}

commit f384dd881d507ba05a8f8578b15fd5218d5fdce1

Deuces wild (#2)
* Add DeucesWild.java

* Created card and hand enums; adjusted payout

* Removed redundant functions in Card.java; updated CardSuit.java

* Rank and suit names and symbols moved to respective enums; game added to Game enum

* Changed board printing algorithm

* Added CardRank.valueOf()

Featuring acesHigh boolean to give an ace its proper value regardless of the game.

* Commented

Added documentation for the getValue() method.

* Adjusted compareTo() method

compareTo() now calls getValue() as well so it gets the right result whether aces are high or low.

* Commenting part 2

Adjusted comments for the methods changed and adjusted comment in the new method to match the same style.

* Fixed an error where compareTo(Card other) was not being overridden

* Added code to generate the deck

* Added update on hand evaluator

* Hand evaluator should be complete now

* Renamed isFullHouse() to hasExtraPair() to clarify what the function is really doing

* Switching out hardcoded enums

Why put them in manually when you can just get the list from the enum itself?

* hasExtraPair() commenting

Adds a comment to explain why this actually works
And also creates a separate array to sort so as not to ruin rankCount for anything else that needs it

* Deck is now its own object

* Got rid of a little more hardcoding

* Commit 87ae03c accidentally undid commit eaed373; this reinstates the change from there

* Moved help text to new initialiseGame() method

* More changes to initialiseGame()

* Completed conversion

* Started work on actually changing over Deuces Wild from Strike it Rich beside new functions :P

* More work done on the game logic

* Last vestige of Strike it Rich from Deuces Wild gone

* Added card display on bottom; fixed formatting bug in Card object

* Readded something I realized I shouldn't have deleted

* playNextMove() updated significantly

src/tel/discord/rtab/enums/CardRank.java
@@ -1,0 +1,49 @@
+package tel.discord.rtab.enums;
+
+// Swiped and slightly modified from another project of mine I did for university. The order of the ranks matter here. --Coug
+public enum CardRank {
+    ACE("Ace","A")
+    {
+    	@Override
+    	public int getValue(boolean acesHigh) {
+    		if(acesHigh)
+    			return this.ordinal() + 14;
+        	return this.ordinal() + 1;
+        }	
+    },
+    DEUCE("Deuce","2"),
+    THREE("Three","3"),
+    FOUR("Four","4"),
+    FIVE("Five","5"),
+    SIX("Six","6"),
+    SEVEN("Seven","7"),
+    EIGHT("Eight","8"),
+    NINE("Nine","9"),
+    TEN("Ten","10"),
+    JACK("Jack","J"),
+    QUEEN("Queen","Q"),
+    KING("King","K");
+    
+    private final String name, symbol;
+
+    CardRank(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+    
+    /**
+     * @param acesHigh  true if aces should be considered the highest card (rather than the lowest)
+     * @return the value of the card - aces are 14 if aces are high, or 1 if aces are low.
+     */
+    public int getValue(boolean acesHigh) {
+    	return this.ordinal() + 1;
+    }
+}

src/tel/discord/rtab/enums/CardSuit.java
@@ -1,0 +1,26 @@
+package tel.discord.rtab.enums;
+
+// Swiped and slightly modified from another project of mine I did for university.
+// Order shouldn't matter *TOO* much here so long as it's also updated in Card.java. --Coug
+
+public enum CardSuit {
+    CLUBS("Clubs","♣"),
+    DIAMONDS("Diamonds","♦"),
+    HEARTS("Hearts","♥"),
+    SPADES("Spades","♠");
+    
+    private final String name, symbol;
+
+    CardSuit(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}

src/tel/discord/rtab/enums/Games.java
@@ -8,6 +8,7 @@ public enum Games implements WeightedSpace {
 	MATH_TIME		(2,"Math Time","Math",new MathTime()),
 	GAMBLE			(2,"The Gamble","Gamble",new Gamble()),
 	THE_OFFER		(2,"The Offer","Offer",new TheOffer()),
+	DEUCES_WILD		(2,"Deuces Wild","Deuces",new DeucesWild()),
 	//Bonus games never appear in the pool
 	SUPERCASH		(0,"SUPERCASH","Super",new Supercash()),
 	DIGITAL_FORTRESS(0,"DIGITAL FORTRESS","Fortress",new DigitalFortress()),

src/tel/discord/rtab/enums/PokerHand.java
@@ -1,0 +1,25 @@
+package tel.discord.rtab.enums;
+
+public enum PokerHand {
+    NOTHING("Nothing"), // couldn't think of any other good name for a losing hand
+    THREE_OF_A_KIND("Three of a Kind"),
+    STRAIGHT("Straight"), // A-2-3-4-5 and 10-J-Q-K-A both count, even though aces are normally high
+    FLUSH("Flush"),
+    FULL_HOUSE("Full House"),
+    FOUR_OF_A_KIND("Four of a Kind"),
+    STRAIGHT_FLUSH("Straight Flush"), // A-2-3-4-5 counts, but not 10-J-Q-K-A -- that's one of the royal hands
+    FIVE_OF_A_KIND("Five of a Kind"), // Only possible with deuces
+    WILD_ROYAL("Wild Royal Flush"),
+    FOUR_DEUCES("Four Deuces"),
+    NATURAL_ROYAL("Natural Royal Flush");
+    
+	private final String name;
+	
+	PokerHand (String name){
+		this.name = name;
+	}
+	
+    public String toString() {
+    	return name;
+    }
+}

src/tel/discord/rtab/minigames/DeucesWild.java
@@ -1,0 +1,381 @@
+package tel.discord.rtab.minigames;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import tel.discord.rtab.enums.CardRank;
+import tel.discord.rtab.enums.CardSuit;
+import tel.discord.rtab.enums.PokerHand;
+import tel.discord.rtab.objs.Card;
+import tel.discord.rtab.objs.Deck;
+
+public class DeucesWild implements MiniGame {
+	static final boolean BONUS = false;
+	static final int BOARD_SIZE = 52;
+	static Deck deck;
+	static Card[] cardsPicked = new Card[5];
+	static boolean[] cardsHeld = new boolean[5];
+	ArrayList<Card> board = new ArrayList<Card>(BOARD_SIZE);
+	int lastSpace;
+	Card lastPicked;
+	PokerHand hand = PokerHand.NOTHING;
+	boolean[] pickedSpaces = new boolean[BOARD_SIZE];
+	boolean firstPlay = true;
+	boolean redrawUsed;
+	byte gameStage;
+
+	@Override
+	public LinkedList<String> initialiseGame()
+	{
+		LinkedList<String> output = new LinkedList<>();
+		//Initialise board
+		board.clear();
+		deck = new Deck();
+		deck.shuffle(); // since the Deck object has its own shuffle method that can be called
+		for(int i=0; i<BOARD_SIZE; i++)
+				board.add(deck.dealCard());
+		cardsPicked = new Card[5];
+		cardsHeld = new boolean[5];
+		pickedSpaces = new boolean[BOARD_SIZE];
+		redrawUsed = false;
+		firstPlay = false;
+		gameStage = 0;
+		//Display instructions
+		output.add("In Deuces Wild, your objective is to obtain the best poker hand possible.");
+		output.add("We have shuffled a standard deck of 52 playing cards, from which you will pick five cards.");
+		output.add("As the name of the game suggests, deuces are wild. Those are always treated as the best card possible.");
+		output.add("After you draw your five cards, you may redraw as many of them as you wish, but only once.");
+		output.add("You must get at least a three of a kind to win any money. That pays $50,000.");
+		output.add("Straights and flushes each pay $100,000. A full house pays $150,000, a four of a kind pays $250,000, "
+				+ "a straight flush pays $450,000, a five of a kind pays $750,000, a wild royal flush pays $1,250,000, "
+				+ "and four deuces pay $10,000,000.");
+		output.add("If you are lucky enough to get a natural royal flush, you will win $40,000,000!");
+		output.add("Best of luck! Pick your first card when you're ready.");
+		output.add(generateBoard());
+		return output;
+	}
+	
+	@Override
+	public LinkedList<String> playNextTurn(String pick)
+	{
+		LinkedList<String> output = new LinkedList<>();
+		
+		if (gameStage == 5) {
+			if (pick.toUpperCase().equals("STOP")) {
+				redrawUsed = true;
+			}
+			else if (pick.toUpperCase().equals("REDRAW")) {
+				output.add("Redrawing all five cards.");
+				redrawUsed = true;
+				gameStage = 0;
+				output.add(generateBoard());
+				output.add("Select your first card of the redraw when you are ready.");
+			}
+			else if (pick.toUpperCase().startsWith("HOLD ")) {
+				String[] tokens = pick.split("\\s");
+				try {
+					// If there are any non-numeric tokens after "HOLD", assume it's just the player talking
+					for (int i = 1; i < tokens.length; i++) {
+						if (!isNumber(tokens[i]))
+							return output;
+					}
+				}
+				catch (IndexOutOfBoundsException e) { // Maybe it's just "hold" by itself :P
+					return output;
+				}
+				
+				// New try-catch block; now we want to make sure the player's choices correspond to actual cards
+				try {
+					for (int i = 1; i < tokens.length; i++) {
+						cardsHeld[Integer.parseInt(tokens[i])-1] = true;
+					}
+					
+					// If we get here, we're good :)
+					redrawUsed = true;
+					gameStage = 0;
+					
+					String cardsHeldAsString = "Cards held: ";
+					String cardsRedrawingAsString = "Cards being redrawn: ";
+					
+
+					for (int i = 0; i < cardsHeld.length; i++) {
+						if (cardsHeld[i])
+							cardsHeldAsString += cardsPicked[i].toStringShort() + " ";
+						else cardsRedrawingAsString += cardsPicked[i].toStringShort() + " ";
+					}
+					
+					if (cardsRedrawingAsString.equals("Cards being redrawn: ")) { // i.e. there aren't any
+						output.add("All five cards held; ending game.");
+						gameStage = 5;
+						return output;
+					}
+					
+					output.add(cardsHeldAsString);
+					output.add(cardsRedrawingAsString);
+					
+					// Find out what stage we should be on now
+					for (int i = 0; i < cardsHeld.length; i++) {
+						if (!cardsHeld[i])
+							break;
+						else gameStage++;
+					}
+					output.add(generateBoard());
+					output.add("Select your first card of the redraw when you are ready.");
+				}
+				catch (IndexOutOfBoundsException e) {
+					// Clear out the hold flags to be safe
+					for (int i = 0; i < cardsHeld.length; i++) {
+						cardsHeld[i] = false;
+					}
+					output.add("Invalid card(s).");
+					return output;
+				}
+			}
+			return output;
+		}
+		else {
+			if(!isNumber(pick))
+			{
+				//Random unrelated non-number doesn't need feedback
+				return output;
+			}
+			if(!checkValidNumber(pick))
+			{
+				output.add("Invalid pick.");
+				return output;
+			}
+			else
+			{
+				lastSpace = Integer.parseInt(pick)-1;
+				pickedSpaces[lastSpace] = true;
+				lastPicked = board.get(lastSpace);
+				cardsPicked[gameStage] = lastPicked;
+				do {
+					gameStage++;
+				} while (gameStage < 5 && cardsHeld[gameStage]);
+				if (gameStage == 5)
+					hand = evaluateHand(cardsPicked);
+				output.add(String.format("Space %d selected...",lastSpace+1));
+				output.add("**" + lastPicked.toString() + "**");
+				output.add(generateBoard());
+				if (gameStage == 5) {
+					if (hand != PokerHand.NATURAL_ROYAL && redrawUsed) {
+						output.add("You may now hold any or all of your five cards by typing HOLD followed by the numeric positions "
+								+"of each card.");
+						output.add("For example, type HOLD 1 to hold only the " + cardsPicked[0] +", or type HOLD 2 5 to hold the "
+								+ cardsPicked[1] + " as well as the " + cardsPicked[4] + ".");
+						output.add("The cards you do not hold will all be redrawn in the hopes of a better hand.");
+						output.add(String.format("If you like your hand, you may also type STOP to end the game and claim your "+
+								"prize of $%,d.", getMoneyWon()));
+						output.add("Alternatively, you can redraw every single card by typing REDRAW.");
+					}
+				}
+				else {
+					output.add("Select another card.");
+				}
+			}
+			return output;
+		}
+	}
+
+	boolean isNumber(String message)
+	{
+		try
+		{
+			//If this doesn't throw an exception we're good
+			Integer.parseInt(message);
+			return true;
+		}
+		catch(NumberFormatException e1)
+		{
+			return false;
+		}
+	}
+	boolean checkValidNumber(String message)
+	{
+		int location = Integer.parseInt(message)-1;
+		return (location >= 0 && location < BOARD_SIZE && !pickedSpaces[location]);
+	}
+	
+	String generateBoard()
+	{
+		StringBuilder display = new StringBuilder();
+		display.append("```\n");
+		display.append("      DEUCES WILD\n   ");
+		for(int i=0; i<BOARD_SIZE; i++)
+		{
+			if(pickedSpaces[i])
+			{
+				display.append("  ");
+			}
+			else
+			{
+				display.append(String.format("%02d",(i+1)));
+			}
+			if (i == 45)
+				display.append("\n   ");
+			else if(i == 5 || (i > 5 && i%8 == 5))
+				display.append("\n");
+			else
+				display.append(" ");
+		}
+		display.append("\n" + "Current hand: "); // The concatenation here is more for human legibility than anything
+		for (int i = 0; i > cardsPicked.length; i++) {
+			if (cardsPicked[i] == null)
+				break;
+			if (redrawUsed && !cardsHeld[i])
+				continue;
+			display.append(cardsPicked[i].toStringShort() + " ");
+		}
+		if (gameStage == 5)
+			display.append("(" + hand.toString() + ")");
+		display.append("```");
+		return display.toString();
+	}
+
+	@Override
+	public boolean isGameOver()
+	{
+		if (gameStage == 5) {
+			if (hand == PokerHand.NATURAL_ROYAL)
+				return true;
+			else return redrawUsed;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public int getMoneyWon()
+	{
+		firstPlay = true;
+		switch(hand) {
+			case NOTHING: return 0;
+			case THREE_OF_A_KIND: return 50000;
+			case STRAIGHT: case FLUSH: return 100000;
+			case FULL_HOUSE: return 150000;
+			case FOUR_OF_A_KIND: return 250000;
+			case STRAIGHT_FLUSH: return 450000;
+			case FIVE_OF_A_KIND: return 750000;
+			case WILD_ROYAL: return 1250000;
+			case FOUR_DEUCES: return 10000000;
+			case NATURAL_ROYAL: return 40000000;
+			default: throw new IllegalArgumentException(); // since the above is supposed to already handle everything
+		}
+	}
+
+	@Override
+	public boolean isBonusGame() {
+		return BONUS;
+	}
+
+	// This is probably not the most efficient way to write the hand evaluator--some things are checked more than once. 
+	public PokerHand evaluateHand(Card[] cards) {
+		if (cards.length != 5)
+			throw new IllegalArgumentException("The hand evaluator function needs 5 cards to work; it was passed " + cards.length + ".");
+
+		byte[] rankCount = new byte[CardRank.values().length];
+		byte[] suitCount = new byte[CardSuit.values().length];
+
+		for (int i = 0; i < cards.length; i++) {
+			rankCount[cards[i].getRank().ordinal()]++;
+			if (cards[i].getRank() != CardRank.DEUCE)      // for the purposes of this evaluator, deuces have no suit; that's the only 
+				suitCount[cards[i].getSuit().ordinal()]++; // way I can think of to get it to work right when checking for a flush
+		}
+
+		// If we have four deuces, that precludes a natural royal flush and outpays a wild royal flush; so it's less work to check for that first
+		if (rankCount[CardRank.DEUCE.ordinal()] == 4)
+			return PokerHand.FOUR_DEUCES;
+
+		CardRank highCardOfStraight = findStraightHighCard(rankCount); // If this is null, we do not have a straight
+		boolean isFlush = isFlush(suitCount);
+
+		// Put off the five of a kind check until these are all done--if we had one, we would have paid higher for four deuces already
+		if (highCardOfStraight != null && isFlush) {
+			if (highCardOfStraight == CardRank.ACE) {
+				if (rankCount[CardRank.DEUCE.ordinal()] == 0)
+					return PokerHand.NATURAL_ROYAL;
+				else return PokerHand.WILD_ROYAL;
+			}
+			else return PokerHand.STRAIGHT_FLUSH;
+		}
+		if (isFlush) return PokerHand.FLUSH;
+		if (highCardOfStraight != null) return PokerHand.STRAIGHT;
+
+		byte modeOfRanks = modeOfRanks(rankCount); // That is, how many are there of the most common rank?
+
+		switch (modeOfRanks) {
+			case 3: 
+				if (hasExtraPair(rankCount)) return PokerHand.FULL_HOUSE;
+				else return PokerHand.THREE_OF_A_KIND;
+			case 4: return PokerHand.FOUR_OF_A_KIND;
+			case 5: return PokerHand.FIVE_OF_A_KIND;
+			default: return PokerHand.NOTHING;
+		}
+	}
+
+	private CardRank findStraightHighCard(byte[] rankCount) {
+		// If we have any paired cards other than deuces, it can't be a straight, so check for that first
+		for (int i = 0; i < rankCount.length; i++) {
+			if (i == CardRank.DEUCE.ordinal())
+				continue;
+			if (rankCount[i] > 1)
+				return null;
+		}
+
+		for (int i = 0; i < rankCount.length - 4; i++) {
+			if (rankCount[i] + rankCount[i+1] + rankCount[i+2] + rankCount[i+3] + rankCount[i+4] == 5)
+				return CardRank.values()[i+4];
+			if (i > CardRank.DEUCE.ordinal() && rankCount[i] + rankCount[i+1] + rankCount[i+2] + 
+					rankCount[i+3] + rankCount[i+4] + rankCount[CardRank.DEUCE.ordinal()] == 5)
+				return CardRank.values()[i+4];
+		}
+
+		// The above scan doesn't catch an ace-high straight; so that is our final check:
+		if (rankCount[CardRank.ACE.ordinal()] + rankCount[CardRank.DEUCE.ordinal()] + rankCount[CardRank.TEN.ordinal()]
+				+ rankCount[CardRank.JACK.ordinal()] + rankCount[CardRank.QUEEN.ordinal()] + rankCount[CardRank.KING.ordinal()] == 5)
+			return CardRank.ACE;
+		
+		return null;
+	}
+
+	private boolean isFlush(byte[] suitCount) {
+		boolean suitFound = suitCount[0] != 0;
+
+		for (int i = 1; i < suitCount.length; i++) {
+			if (suitCount[i] != 0) {
+				if (suitFound) return false;
+				else suitFound = true;
+			}
+		}
+
+		return true;
+	}
+
+	private byte modeOfRanks(byte[] rankCount) {
+		byte deuces = rankCount[CardRank.DEUCE.ordinal()];
+		byte max = (byte)(rankCount[CardRank.ACE.ordinal()] + deuces);
+
+		for (int i = CardRank.THREE.ordinal(); i < rankCount.length; i++) {
+			byte sum = (byte)(rankCount[i] + deuces);
+			if (sum > max)
+				max = sum;
+		}
+
+		return max;	
+	}
+
+	private boolean hasExtraPair(byte[] rankCount) {
+		/* 
+ 		 * This works, but isn't entirely clear why:
+ 		 * There can only be a maximum of one deuce in a full house
+ 		 * And if there is one, it's pair + pair + deuce
+ 		 * Otherwise the result would be four or five of a kind and we wouldn't get to this point anyway
+  		 * In any case, in a full house sorting the array comes out as either (0,2,3) or (1,2,2)
+ 		 * And the second-to-last value would always be 2.
+ 		 */
+ 		byte[] sortedRankCount = rankCount;
+ 		Arrays.sort(sortedRankCount);
+ 		return sortedRankCount[rankCount.length - 2] == 2;
+	}
+}

src/tel/discord/rtab/objs/Card.java
@@ -1,0 +1,109 @@
+package tel.discord.rtab.objs;
+
+import java.util.Objects;
+import tel.discord.rtab.enums.CardRank;
+import tel.discord.rtab.enums.CardSuit;
+
+// Swiped and slightly modified from another project of mine I did for university. --Coug
+
+public class Card implements Comparable<Card> {
+    private final CardRank rank;
+    private final CardSuit suit;
+    
+    /**
+     * Sole constructor.
+     * 
+     * @param rank  the card's rank
+     * @param suit  the card's suit
+     */
+    public Card(CardRank rank, CardSuit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    
+    public CardRank getRank() {
+        return rank;
+    }
+    
+    public CardSuit getSuit() {
+        return suit;
+    }
+
+    /**
+     * Checks whether this card outranks the card in the argument. This method,
+     * unlike {@code compareTo(Card o, boolean acesHigh)}, ignores suit, eliminating some of the
+     * overhead from the {@code compareTo(Card o, boolean acesHigh)} method.
+     * 
+     * @param other  the card to be compared to
+     * @param acesHigh  true if aces should be considered the highest card (rather than the lowest)
+     * @return true if the object card is higher in rank than the argument card,
+     * false otherwise
+     */
+    public boolean outranks(Card other, boolean acesHigh) {
+        if (other == null)
+            throw new NullPointerException();
+        
+        return this.rank.getValue(acesHigh) > other.getRank().getValue(acesHigh);
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.rank);
+        hash = 59 * hash + Objects.hashCode(this.suit);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        
+        if (getClass() != obj.getClass())
+            return false;
+        
+        final Card other = (Card) obj;
+        
+        return this.rank == other.getRank() && this.suit == other.getSuit();
+    }
+    
+    @Override
+    public int compareTo(Card other) {
+        return compareTo(other, false);
+    }
+
+    /**
+     * This function is intended for sorting purposes. For simply determining if
+     * one card is higher in rank than another, {@code outranks(Card other, boolean acesHigh)}
+     * should be called instead as this method also takes suits into account.
+     * <p>
+     * For the purposes of this method, if two cards are of different suits,
+     * then the card of the suit listed second in {@code CardSuit} is the
+     * higher card. If two cards have the same suit, then this card is
+     * higher than the card in the argument if {@code outranks(Card other, boolean acesHigh)}
+     * would return true.
+     * 
+     * @param other  the card to be compared to
+     * @param acesHigh  true if aces should be considered the highest card (rather than the lowest)
+     * @return a positive number if this card is higher than the card in the
+     * argument, a negative number if this card is lower than the card in the
+     * argument, and 0 if the two cards are identical
+     */
+    public int compareTo(Card other, boolean acesHigh) {
+        if (other == null)
+            throw new NullPointerException();
+        
+        return (this.suit.ordinal() * CardRank.values().length +
+                this.rank.getValue(acesHigh)) - (other.getSuit().ordinal() *
+                CardRank.values().length + other.getRank().getValue(acesHigh));
+    }
+    
+    @Override
+    public String toString() {
+        return this.rank.getName() + " of " + this.suit.getName();
+    }
+    
+    public String toStringShort() {
+        return this.rank.getSymbol() + this.suit.getSymbol();
+    }
+}

src/tel/discord/rtab/objs/Deck.java
@@ -1,0 +1,42 @@
+package tel.discord.rtab.objs;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import tel.discord.rtab.enums.CardRank;
+import tel.discord.rtab.enums.CardSuit;
+
+public class Deck {
+	LinkedList<Card> deck;
+
+	public Deck() {
+		this(1);
+	}
+
+	public Deck(int numDecks) {
+		deck = new LinkedList<>();
+		addDecks(numDecks);
+	}
+
+	public void addDecks(int numDecks) {
+		CardRank[] ranks = CardRank.values();
+		CardSuit[] suits = CardSuit.values();
+		
+		for (int i = 0; i < numDecks; i++) {
+			for (int j = 0; j < ranks.length * suits.length; j++) {
+				deck.add(new Card(ranks[j/suits.length],suits[j%suits.length]));
+			}
+		}
+	}
+
+	public void clear() {
+		deck.clear();
+	}
+
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+
+	public Card dealCard() {
+		return deck.pop();
+	}
+}