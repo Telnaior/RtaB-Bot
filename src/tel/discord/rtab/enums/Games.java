216fd1a6ea485b4a4eb42161d23ae38dfbf92da9
Telnaior <Telnaior@users.noreply.github.com>
2019-06-11 08:38:10 +1000

    Add Game List "rotated out" section
    So that we can clearly see what's currently in the pool and what's not, and so that we don't just end up with a bunch of games commented out (as the one currently commented out is gone for technical reasons)

--- src/tel/discord/rtab/enums/Games.java
+++ src/tel/discord/rtab/enums/Games.java
@@ -8,18 +8,19 @@ public enum Games implements WeightedSpace {
 	STRIKE_IT_RICH		(2,"Strike it Rich","Strike") { public MiniGame getGame() { return new StrikeItRich(); } },					//Author: Atia
 	GAMBLE				(1,"The Gamble","Gamble") { public MiniGame getGame() { return new Gamble(); } },							//Author: Atia
 	TRIPLE_PLAY			(1,"Triple Play","Triple") { public MiniGame getGame() { return new TriplePlay(); } },						//Author: Atia
-	//THE_OFFER			(2,"The Offer","Offer") { public MiniGame getGame() { return new TheOffer(); }	},							//Author: Amy
 	DEUCES_WILD			(2,"Deuces Wild","Deuces") { public MiniGame getGame() { return new DeucesWild(); } },						//Author: StrangerCoug
 	DOUBLE_TROUBLE		(2,"Double Trouble","Double") { public MiniGame getGame() { return new DoubleTrouble(); } },				//Author: JerryEris
 	DEAL_OR_NO_DEAL		(2,"Deal or No Deal","DoND") { public MiniGame getGame() { return new DealOrNoDeal(); } },					//Author: Atia
-	//BUMPER_CASH   	(2,"Bumper Grab","Bumper") { public MiniGame getGame() { return new BumperGrab(); } },						//Author: Tara
 	MINEFIELD_MULTIPLIER(2,"Minefield Multiplier","Multiplier") {public MiniGame getGame() { return new MinefieldMultiplier(); } }, //Author: Amy
 	DOUBLE_ZERO     	(2,"Double Zero","00") { public MiniGame getGame() { return new DoubleZeroes(); } },						//Author: JerryEris
 	SHUT_THE_BOX		(2,"Shut the Box","Shut") { public MiniGame getGame() { return new ShutTheBox(); } },						//Author: StrangerCoug
 	COIN_FLIP			(2,"CoinFlip","Flip") { public MiniGame getGame() { return new CoinFlip(); } },								//Author: Amy
 	BOMB_ROULETTE		(2,"Bomb Roulette","Roulette") { public MiniGame getGame() { return new BombRoulette(); } },				//Author: StrangerCoug
 	HI_LO_DICE			(2,"Hi/Lo Dice","Hi/Lo") { public MiniGame getGame() { return new HiLoDice(); } },							//Author: StrangerCoug
-	//Bonus games ne	ver appear in the pool
+	//Games rotated out
+	THE_OFFER			(0,"The Offer","Offer") { public MiniGame getGame() { return new TheOffer(); }	},							//Author: Amy
+	//BUMPER_CASH   	(2,"Bumper Grab","Bumper") { public MiniGame getGame() { return new BumperGrab(); } },						//Author: Tara
+	//Bonus games never appear in the pool
 	SUPERCASH			(0,"SUPERCASH","Super") { public MiniGame getGame() { return new Supercash(); } },
 	DIGITAL_FORTRESS	(0,"DIGITAL FORTRESS","Fortress") { public MiniGame getGame() { return new DigitalFortress(); } },
 	SPECTRUM			(0,"SPECTRUM","Spectrum") { public MiniGame getGame() { return new Spectrum(); } },
