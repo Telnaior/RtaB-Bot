0cb7dcd82de0d8c0e266d70a800eefeeb49634f2
Telnaior <JDGA@iinet.net.au>
2019-07-23 09:04:15 +1000

    Updated Games enum
    Added Call Your Shot to the enum, picked two games to be rotated out to keep the total in the pool at 12, and added the SBR as well so that the Minigame Test can pull from it.
    
    For others: The easiest way to avoid SBR-related compilation errors is to copy another minigame's file into its slot.

--- src/tel/discord/rtab/enums/Games.java
+++ src/tel/discord/rtab/enums/Games.java
@@ -5,9 +5,7 @@ import tel.discord.rtab.minigames.*;
 public enum Games implements WeightedSpace {
 	//Minigame Pool
 	MATH_TIME			(2,"Math Time","Math") { public MiniGame getGame() { return new MathTime(); } },							//Author: Atia
-	STRIKE_IT_RICH		(2,"Strike it Rich","Strike") { public MiniGame getGame() { return new StrikeItRich(); } },					//Author: Atia
-	GAMBLE				(1,"The Gamble","Gamble") { public MiniGame getGame() { return new Gamble(); } },							//Author: Atia
-	TRIPLE_PLAY			(1,"Triple Play","Triple") { public MiniGame getGame() { return new TriplePlay(); } },						//Author: Atia
+	TRIPLE_PLAY			(2,"Triple Play","Triple") { public MiniGame getGame() { return new TriplePlay(); } },						//Author: Atia
 	DEUCES_WILD			(2,"Deuces Wild","Deuces") { public MiniGame getGame() { return new DeucesWild(); } },						//Author: StrangerCoug
 	DOUBLE_TROUBLE		(2,"Double Trouble","Double") { public MiniGame getGame() { return new DoubleTrouble(); } },				//Author: JerryEris
 	DEAL_OR_NO_DEAL		(2,"Deal or No Deal","DoND") { public MiniGame getGame() { return new DealOrNoDeal(); } },					//Author: Atia
@@ -15,16 +13,21 @@ public enum Games implements WeightedSpace {
 	DOUBLE_ZERO     	(2,"Double Zero","00") { public MiniGame getGame() { return new DoubleZeroes(); } },						//Author: JerryEris
 	SHUT_THE_BOX		(2,"Shut the Box","Shut") { public MiniGame getGame() { return new ShutTheBox(); } },						//Author: StrangerCoug
 	COIN_FLIP			(2,"CoinFlip","Flip") { public MiniGame getGame() { return new CoinFlip(); } },								//Author: Amy
-	BOMB_ROULETTE		(1,"Bomb Roulette","Roulette") { public MiniGame getGame() { return new BombRoulette(); } },				//Author: StrangerCoug
+	BOMB_ROULETTE		(2,"Bomb Roulette","Roulette") { public MiniGame getGame() { return new BombRoulette(); } },				//Author: StrangerCoug
 	HI_LO_DICE			(2,"Hi/Lo Dice","Hi/Lo") { public MiniGame getGame() { return new HiLoDice(); } },							//Author: StrangerCoug
+	CALL_YOUR_SHOT		(2,"Call Your Shot","Call") { public MiniGame getGame() { return new CallYourShot(); } },					//Author: JerryEris
 	//Games rotated out
 	THE_OFFER			(0,"The Offer","Offer") { public MiniGame getGame() { return new TheOffer(); }	},							//Author: Amy
+	STRIKE_IT_RICH		(0,"Strike it Rich","Strike") { public MiniGame getGame() { return new StrikeItRich(); } },					//Author: Atia
+	GAMBLE				(2,"The Gamble","Gamble") { public MiniGame getGame() { return new Gamble(); } },							//Author: Atia
 	//BUMPER_CASH   	(2,"Bumper Grab","Bumper") { public MiniGame getGame() { return new BumperGrab(); } },						//Author: Tara
 	//Bonus games never appear in the pool
 	SUPERCASH			(0,"SUPERCASH","Super") { public MiniGame getGame() { return new Supercash(); } },
 	DIGITAL_FORTRESS	(0,"DIGITAL FORTRESS","Fortress") { public MiniGame getGame() { return new DigitalFortress(); } },
 	SPECTRUM			(0,"SPECTRUM","Spectrum") { public MiniGame getGame() { return new Spectrum(); } },
-	HYPERCUBE			(0,"HYPERCUBE","Hyper^3") { public MiniGame getGame() { return new Hypercube(); } };
+	HYPERCUBE			(0,"HYPERCUBE","Hyper^3") { public MiniGame getGame() { return new Hypercube(); } },
+	//This game also never appears in the pool, for obvious reasons
+	SUPER_BONUS_ROUND	(0,null,"sbr") {public MiniGame getGame() { return new SuperBonusRound(); } };
 	
 	String fullName;
 	String shortName;
@@ -44,7 +47,6 @@ public enum Games implements WeightedSpace {
 	{
 		return shortName;
 	}
-	
 	//Returns a new instance of the requested minigame
 	public abstract MiniGame getGame();
 	
