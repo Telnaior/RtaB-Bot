9d339c945b7c5b94cd352ea03a26c1961e531547
Jerry <JerryRules@gmail.com>
2019-08-04 22:26:15 -0500

    Prizes File!
    And trophies are now prizes

---
+++ src/tel/discord/rtab/enums/PrizeType.java
@@ -0,0 +1,30 @@
+package tel.discord.rtab.enums;
+
+public enum PrizeType {
+	//TROPHIES, woo winners
+	S1TROPHY (   68000,1,"a replica of Vash's Season 1 trophy"),
+	S2TROPHY (   60000,1,"a replica of Charles510's Season 2 trophy"),
+	S3TROPHY (   54000,1,"a replica of Archstered's Season 3 trophy"),
+	
+	//Regular prizes
+	DB1		    (   22805,"a DesertBuck"),
+	DANGAN    (   11037,"an unfamiliar memory"),
+	GOVERNOR  (   26000,"the Governor's favourite"),
+	BIGJON    (    1906,"the BigJon special");
+    
+    private final int theValue;
+    private final String theName;
+
+    PrizeType(int theValue, String theName) {
+        this.value = theValue;
+        this.name = theName;
+    }
+
+    public String getPrizeName() {
+        return name;
+    }
+
+    public int getPrizeValue() {
+        return value;
+    }
+}
