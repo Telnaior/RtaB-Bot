1d8c815c9fe449b3bda24f7ce74403dc15d873e2
Telnaior <JDGA@iinet.net.au>
2020-05-02 13:29:05 +1000

    Add potential for cash that only multiplies by the winstreak
    There's a decent chance that this will come in handy.

--- src/tel/discord/rtab/enums/MoneyMultipliersToUse.java
+++ src/tel/discord/rtab/enums/MoneyMultipliersToUse.java
@@ -3,6 +3,7 @@ package tel.discord.rtab.enums;
 public enum MoneyMultipliersToUse {
 	NOTHING(false,false),
 	BOOSTER_ONLY(true,false),
+	BONUS_ONLY(false,true),
 	BOOSTER_AND_BONUS(true,true);
 	
 	public boolean useBoost, useBonus;
